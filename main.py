77379b3030f6df312a60141e5424817ceb1ffc84
JakubPiwowarczyk <s24820@pjwstk.edu.pl>
2023-04-10 22:24:02 +0200

    created simple module myModule

---
+++ main.py
@@ -0,0 +1,7 @@
+import myModule
+
+if __name__ == '__main__':
+    print(dir(myModule))
+    myModule.say_hi("Kuba")
+    myModule.my_age(21)
+    
---
+++ myModule.py
@@ -0,0 +1,6 @@
+def say_hi(name):
+    print("Hi " + name)
+
+
+def my_age(age):
+    print(f'You are {age} years old!')
