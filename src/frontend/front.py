2c0428e19e7b2a952c617b152fa2fb057336a403
JakubPiwowarczyk <s24820@pjwstk.edu.pl>
2023-06-03 19:31:15 +0200

    added static folder with css and js files to frontend, created homepage

--- src/frontend/front.py
+++ src/frontend/front.py
@@ -8,12 +8,18 @@ RESTAPI_URL = "http://127.0.0.1:8000/api"
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="src/frontend/static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-templates = Jinja2Templates(directory="src/frontend/templates")
 
-
-@app.get("/home-page", response_class=HTMLResponse)
+@app.get("/blog/home", response_class=HTMLResponse)
 async def get_home_page(req: Request):
-    user = requests.get(f'{RESTAPI_URL}/getUser/17')
-    return templates.TemplateResponse("index.html", {"request": req, "user": user.json()})
+    blogs = requests.get(f'{RESTAPI_URL}/getNMostPopularBlogs?amount_to_display=10')
+    return templates.TemplateResponse("index.html", {"request": req, "blogs": blogs.json()})
+
+
+@app.get("/blog/blog-page/{blog_id}}", response_class=HTMLResponse)
+async def get_blog_page(req: Request, blog_id: int):
+    blog = requests.get(f'{RESTAPI_URL}/getBlog/{blog_id}')
+    posts = requests.get(f'{RESTAPI_URL}/getBlogPosts/{blog_id}')
+    return templates.TemplateResponse("blog.html", {"request": req, "blog": blog, "posts": posts})
